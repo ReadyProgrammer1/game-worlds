2de48c6ea10729a02759b5b09c3c1cdaed014a26
Daniel Payne <42071158+ReadyProgrammer1@users.noreply.github.com>
2018-11-07 11:16:40 -0500

    Add files via upload

---
+++ precache-manifest.0c6a20c6128ead7bef52608889942365.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "acb53a6ba86d8f3d74fbacd4e4728ad0",
+    "url": "/game-worlds/static/media/goldensparkles.acb53a6b.gif"
+  },
+  {
+    "revision": "a01f227ee3bc3ff659cc",
+    "url": "/game-worlds/static/css/main.1d389647.chunk.css"
+  },
+  {
+    "revision": "1ace49e86b6e802d16ec",
+    "url": "/game-worlds/static/js/1.1ace49e8.chunk.js"
+  },
+  {
+    "revision": "69a2e4027bdcb24298fb",
+    "url": "/game-worlds/static/js/runtime~main.69a2e402.js"
+  },
+  {
+    "revision": "9f19440f74f1c8126fdcf003e3d024da",
+    "url": "/game-worlds/static/media/player_walk.9f19440f.png"
+  },
+  {
+    "revision": "f78c74748dd218cced7f9c4f8a9ae729",
+    "url": "/game-worlds/static/media/player_walk.f78c7474.png"
+  },
+  {
+    "revision": "0a070dc989745d93da76be24cb162c4d",
+    "url": "/game-worlds/static/media/player_red-armor.0a070dc9.png"
+  },
+  {
+    "revision": "3f9c9a560a2c7383c934ed3b5a7b8fd4",
+    "url": "/game-worlds/static/media/player_blue-armor.3f9c9a56.png"
+  },
+  {
+    "revision": "7df6e67f834c347beb55a857f526e59b",
+    "url": "/game-worlds/static/media/yacht.7df6e67f.png"
+  },
+  {
+    "revision": "f9fa784583b98a8eceffeb371c4f80dd",
+    "url": "/game-worlds/static/media/castle.f9fa7845.png"
+  },
+  {
+    "revision": "eb321eb36d72c9a065615e231150eb55",
+    "url": "/game-worlds/static/media/volcano.eb321eb3.png"
+  },
+  {
+    "revision": "fdf2a113814a6c1b824465e9e9c56aa0",
+    "url": "/game-worlds/static/media/forest.fdf2a113.png"
+  },
+  {
+    "revision": "b6c1ed93ce0f44a88c54bbb4485894c9",
+    "url": "/game-worlds/static/media/birds-of-paradise.b6c1ed93.png"
+  },
+  {
+    "revision": "5e50304a015ee7814b239d833b9a4ddd",
+    "url": "/game-worlds/static/media/dragons.5e50304a.png"
+  },
+  {
+    "revision": "4638f0a02866c33c97e81d26d940d2cb",
+    "url": "/game-worlds/static/media/football.4638f0a0.png"
+  },
+  {
+    "revision": "bf6e4e5279b42fbb160092f17c6d71d3",
+    "url": "/game-worlds/static/media/soccer.bf6e4e52.png"
+  },
+  {
+    "revision": "02665d5305546e1d18081a45eed25359",
+    "url": "/game-worlds/static/media/purplespaceship.02665d53.png"
+  },
+  {
+    "revision": "f2312ba2c34913acaf7dc54a8f7bcb12",
+    "url": "/game-worlds/static/media/zoo.f2312ba2.png"
+  },
+  {
+    "revision": "a01f227ee3bc3ff659cc",
+    "url": "/game-worlds/static/js/main.a01f227e.chunk.js"
+  },
+  {
+    "revision": "47ee47c4f269b2bd9d0dc942d20a51c8",
+    "url": "/game-worlds/static/media/treasurebox.47ee47c4.gif"
+  },
+  {
+    "revision": "41534e3f9d18773ea1c6921d048b9bf1",
+    "url": "/game-worlds/static/media/angular.41534e3f.png"
+  },
+  {
+    "revision": "6236315a71aafa731e47e8b257209665",
+    "url": "/game-worlds/static/media/javascript.6236315a.png"
+  },
+  {
+    "revision": "45b8c0f3e76f3ca77803e77d6e939ef0",
+    "url": "/game-worlds/static/media/rails.45b8c0f3.png"
+  },
+  {
+    "revision": "8d525ef06ae4fffe665808a7645b2d87",
+    "url": "/game-worlds/static/media/react.8d525ef0.png"
+  },
+  {
+    "revision": "0902c5f0f88e923f5fcea7f2015c2337",
+    "url": "/game-worlds/static/media/reactu.0902c5f0.png"
+  },
+  {
+    "revision": "df2c2100f779713c0144da20c05d47de",
+    "url": "/game-worlds/static/media/redux.df2c2100.png"
+  },
+  {
+    "revision": "078e4e21ed04b234ab5f45ef362b3d1a",
+    "url": "/game-worlds/static/media/ruby.078e4e21.png"
+  },
+  {
+    "revision": "1f39867e0721c6a5056698cbedce0a89",
+    "url": "/game-worlds/static/media/guerilla.1f39867e.png"
+  },
+  {
+    "revision": "e9d691f4ca2446a00d3e76d48ada33a0",
+    "url": "/game-worlds/static/media/elephant.e9d691f4.png"
+  },
+  {
+    "revision": "fcfa6f25aaf15edbe50888a98fc11c31",
+    "url": "/game-worlds/static/media/giraffe.fcfa6f25.png"
+  },
+  {
+    "revision": "2576961d6de3d7420e65ec564c630f0a",
+    "url": "/game-worlds/static/media/lion.2576961d.png"
+  },
+  {
+    "revision": "22be6c2a2c7add9ae9572937276ef697",
+    "url": "/game-worlds/static/media/hippo.22be6c2a.png"
+  },
+  {
+    "revision": "4923a445502dede1016e65e156672e04",
+    "url": "/game-worlds/static/media/toucan.4923a445.png"
+  },
+  {
+    "revision": "b1389bc0757905bd9614648630ba9542",
+    "url": "/game-worlds/static/media/panda.b1389bc0.png"
+  },
+  {
+    "revision": "9e66e01ee7432ccc43b957a38992990b",
+    "url": "/game-worlds/static/media/rhino.9e66e01e.png"
+  },
+  {
+    "revision": "b8a1266074da00be2b496a7647205837",
+    "url": "/game-worlds/index.html"
+  }
+];
